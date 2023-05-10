a044417d15f137302e86fadd76b270c1312d6bfd
Justin Sixsmith <justinsixsmith@gmail.com>
2023-05-10 09:44:57 -0500

    Imports, displays, and removes reminders

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
-
-const reminders = [
-  {
-    id: 1,
-    title: "Reminder 1",
-  },
-];
+import Reminder from "./components/models/reminder";
+import reminderService from "./services/reminder";
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
+
+  const removeReminder = async (id: number) =>
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+
   return (
     <div className="App">
-      <ReminderList items={reminders} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
